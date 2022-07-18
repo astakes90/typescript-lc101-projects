99fd0e53ebb3ac0f39afc3b98a9dd89db9051c2f
Annalyssa Stakes <annalyssastakes@Annalyssas-Air.attlocal.net>
2022-07-17 19:29:54 -0500

    completed studio

---
+++ studio/Rocket.ts
@@ -0,0 +1,55 @@
+import { Astronaut } from "./Astronaut";
+import { Cargo } from "./Cargo";
+import { Payload } from "./Payload";
+
+export class Rocket {
+    name: string;
+    totalCapacityKg: number;
+    cargoItems: Cargo[];
+    astronauts: Astronaut[];
+
+    constructor(name: string, totalCapacityKg: number) {
+        this.name = name;
+        this.totalCapacityKg = totalCapacityKg;
+
+    }
+
+    sumMass( items: Payload[] ): number {
+        let total = 0
+        for (let index=0; index<items.length; index++) {
+            total += index;
+        } return total;
+    }
+
+    currentMassKg(): number {
+        let totalCurrentMass = 0;
+        totalCurrentMass = this.sumMass(this.astronauts) + this.sumMass(this.cargoItems);
+        return totalCurrentMass;
+    }
+
+    canAdd(item: Payload): boolean {
+        if (this.currentMassKg() + item.massKg <= this.totalCapacityKg) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    addCargo(cargo: Cargo): boolean {
+        if (this.canAdd(cargo) === true) {
+            this.cargoItems.push(cargo);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    addAstronaut(astronaut: Astronaut): boolean {
+        if (this.canAdd(astronaut) === true) {
+            this.astronauts.push(astronaut);
+            return true;
+        } else {
+            return false;
+        }
+    }
+ }
